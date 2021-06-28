f0e834e56eae7efd48835c54aea935c21d73f232
kjaehyeon <jacob9404@gmail.com>
2021-06-29 04:27:02 +0900

    CBV적용 & 부트스트랩적용

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,33 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from . models import Post
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    #views.py에서 db에 쿼리를 날려 원하는 레코드 가져옴
-    #oreder_by()로 최신순으로 post나열
-
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts' : posts,
-        }
-    )
-
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post':post,
-        }
-    )
+class PostList(ListView):
+    model = Post
+    ordering = '-pk' #최신순 포스팅
+    #template_name = 'blog/post_list.html'
+    #Post_list.html파일을 만들거나 template_name을 지정해준다.
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#     #views.py에서 db에 쿼리를 날려 원하는 레코드 가져옴
+#     #oreder_by()로 최신순으로 post나열
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts' : posts,
+#         }
+#     )
+class single_post_page(DetailView):
+    model = Post
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post':post,
+#         }
+#     )
