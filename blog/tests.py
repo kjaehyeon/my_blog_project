a44c45e36fff8558dcaa42333b5589d8974a6ec0
kjaehyeon <jacob9404@gmail.com>
2021-07-03 03:37:50 +0900

    템플릿 모듈화하기

--- blog/tests.py
+++ blog/tests.py
@@ -14,10 +14,7 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
         #1.4 네비게이션 바가 있다.
-        navbar = soup.nav
-        #1.5 Blog, About Me 라는 문구가 내비게이션 바에 있다.
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.nav_bar_test(soup)
 
         #2.1 메인 영역에 게시물이 하나도 없다면
         self.assertEqual(Post.objects.count(), 0)
@@ -45,3 +42,47 @@ class TestView(TestCase):
         self.assertIn(post_002.title, main_area.text)
         #3.4 아직 게시물이 없습니다 라는 문구는 더이상 보이지 않는다.
         self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
+
+    def test_post_detail(self):
+        #1.1 포스트가 하나 있다.
+        post_001 = Post.objects.create(
+            title = '첫 번째 포스트',
+            content = '첫 번째 글'
+        )
+        #1.2 포스트의 url은 '/blog/1/'이다
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+
+        #2 포스트의 상세페이지 테스트
+        #2.1 첫번째 포스트의 url로 접근하면 정상작동한다.
+        response = self.client.get(post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        #2.2 포스트 목록 페이지와 같은 네비게이션 바가 있다.
+        self.nav_bar_test(soup)
+        #2.3 첫번째 포스트의 제목이 브라우저 탭 타이틀에 있다.
+        self.assertIn(post_001.title,soup.title.text)
+        #2.4 첫 번째 포스트의 제목이 post_area에 있다.
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(post_001.title, post_area.text)
+        #2.5 첫번째 포스트의 작성자가 포스트 영역에 있다.
+        #2.6 첫번째 포스트의 내용이 포스트 영역에 있다.
+        self.assertIn(post_001.content, post_area.text)
+
+    def nav_bar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', text='Do it Django')
+        self.assertEqual(logo_btn.attrs['href'] ,'/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_btn.attrs['href'], '/about_me/')
+
